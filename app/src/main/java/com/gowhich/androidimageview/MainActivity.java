912960342696974cb0eaea873d3e96a61413837d
dapeng126 <zhangdapeng89@126.com>
2016-09-02 11:31:17 +0800

    添加 ImageView 基本使用 code

--- app/src/main/java/com/gowhich/androidimageview/MainActivity.java
+++ app/src/main/java/com/gowhich/androidimageview/MainActivity.java
@@ -2,6 +2,8 @@ package com.gowhich.androidimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView1 = (ImageView) this.findViewById(R.id.imageView1);
+
+
+        imageView1.setLayoutParams(new LinearLayout.LayoutParams(200, 100));
+        setTitle("height: " + imageView1.getLayoutParams().height + " Width: " + imageView1.getLayoutParams().width);
     }
 }
